13f34f162a92661218d742a84c2fd89f82e9fdb6
xiaoxiongmeipigu <zouwei_13146216093@163.com>
2016-09-23 15:25:56 +0800

    初步建立项目

---
+++ OpenTable/app/src/main/java/com/yigu/opentable/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yigu.opentable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
